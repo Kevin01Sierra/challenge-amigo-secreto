faa66310eaa2a22381d205d5d2db861773954850
Kevin01Sierra <kevinnsg1602@gmail.com>
2025-01-30 00:47:37 -0500

    Comenta la funcionalidad de actualizarAmigos e implementa la función sortearAmigo

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//lista de amigos
 let amigos = [];
 
 // Implementamos una función para agregar amigos
@@ -26,16 +27,38 @@ function agregarAmigo(){
 
 // Implementamos una funcion para limpiar la caja de texto
 function limpiarCaja(){
+    // Limpiamos el campo de texto
     document.querySelector('#amigo').value = '';
 }
 
 // Implementamos una función para actualizar la lista de amigos
 function actualizarListaAmigos(){
+    // Obtenemos la lista de amigos
     let lista = document.getElementById('listaAmigos');
+    // Limpiamos la lista de amigos
     lista.innerHTML = "";
+    // Recorremos la lista de amigos
     for (let amigo of amigos){
+        // Creamos un elemento li
         let li = document.createElement('li');
+        // Agregamos el nombre del amigo al elemento li
         li.textContent = amigo;
+        // Agregamos el amigo a la lista
         lista.appendChild(li);
     }
+}
+
+// Implementamos una función para sortear los amigos
+function sortearAmigo(){
+    // Validamos que la lista de amigos no esté vacía
+    if(amigos.length === 0){
+        alert("No hay amigos en la lista.");
+        return;
+    }
+    // Obtenemos un número aleatorio
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    // Obtenemos el amigo sorteado
+    let amigoSorteado = amigos[indiceAleatorio];
+    // Mostramos el amigo sorteado
+    document.getElementById('resultado').textContent = `El amigo secreto sorteado es: ${amigoSorteado}`;
 }
